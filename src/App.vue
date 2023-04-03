dd2638d3dd28dd7554f524373dce1e4430c94c26
mikaeltreppo <mikaeltreppo@outlook.com>
2023-04-03 12:42:05 +0200

    inserimento serie

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
 import axios from 'axios';
 import { store } from '../store.js';
 import MyMovieList from './components/MyMovieList.vue';
-import MySearchBar from './components/MySearchBar.vue'
+import MySearchBar from './components/MySearchBar.vue';
+import MySeriesList from './components/MySeriesList.vue';
 
 export default {
   data() {
@@ -12,38 +13,59 @@ export default {
   },
 
   methods: {
+    //Film
     getMovie() {
       store.loading = true;
 
-
       let urlApi = "https://api.themoviedb.org/3/search/movie?api_key=e5909ae0f4f96e86e199022bd90ac5cf"
       if (store.search.length > 0) {
-          urlApi += `&query=${store.search}`;
-        }
-
+        urlApi += `&query=${store.search}`;
+      }
 
       axios.get(urlApi)
         .then(response => {
           store.filmList = response.data.results;
+       
+        })
+    },
+    //serie Tv
+    getSeries() {
+ 
+
+
+      let urlApi = "https://api.themoviedb.org/3/search/tv?api_key=e5909ae0f4f96e86e199022bd90ac5cf"
+      if (store.search.length > 0) {
+        urlApi += `&query=${store.search}`;
+      }
+
+
+      axios.get(urlApi)
+        .then(response => {
+          console.log(response)
+          store.seriesList = response.results;
           store.loading = false;
         })
     }
   },
   components: {
     MyMovieList,
+    MySeriesList,
     MySearchBar
   },
   created() {
     this.getMovie();
+    this.getSeries();
   }
 }
 </script>
 
 <template>
-  <MySearchBar @doSearch="getMovie"/>
+  <MySearchBar @doSearch="getMovie" />
 
-
+  <h2>Film</h2>
   <MyMovieList />
+  <h2>Serie Tv</h2>
+  <MySeriesList />
 </template>
 
 <style scoped></style>
---
+++ src/components/MySeriesList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-if="store.loading == false">
+          <MyFilmCard v-for="(card, index) in store.seriesList" :title="card.title" :originalTitle="card.original_title" :language="card.original_language" :vote="card.vote_average"   :key="index"></MyFilmCard>
+      </div>
+      <div v-else>La pagina sta caricando</div>
+     
+  </template>
+  <script>
+  import { store } from '../../store.js';
+  import MyFilmCard from './MyFilmCard.vue';
+  export default {
+      name: 'MySeriesList',
+      data() {
+          return {
+              store
+          }
+      },
+      components: {
+          MyFilmCard
+      }
+  
+  }
+  
+  
+  </script>
+  <style></style>

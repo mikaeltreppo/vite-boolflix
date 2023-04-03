570edc0778a99841c10345c704dacd1126370230
mikaeltreppo <mikaeltreppo@outlook.com>
2023-04-03 12:08:32 +0200

    aggiunta 4 bandiere

--- src/components/MyFilmCard.vue
+++ src/components/MyFilmCard.vue
@@ -2,7 +2,15 @@
     <div>
         {{ title }}
         {{ originalTitle }}
-        {{ language }}
+        <span v-if="this.language == 'it'"><img class="flags"
+                src="https://seekflag.com/app/uploads/2021/12/Flag-of-Italy-01-1.svg" alt=""> </span>
+        <span v-if="this.language == 'en'"><img class="flags"
+                src="https://seekflag.com/app/uploads/2021/11/Flag-of-Great-Britain-01-1.svg" alt=""> </span>
+        <span v-if="this.language == 'es'"><img class="flags"
+                src="https://seekflag.com/app/uploads/2021/12/Flag-of-Spain-01-2.svg" alt=""> </span>
+        <span v-if="this.language == 'de'"><img class="flags"
+                src="https://seekflag.com/app/uploads/2021/11/Flag-of-Germany-01-1.svg" alt=""> </span>
+        <span v-else>{{ language }}</span>
         {{ vote }}
     </div>
 </template>
@@ -19,8 +27,8 @@ export default {
     props: {
         title: String,
         originalTitle: String,
-        language:String,
-        vote:Number
+        language: String,
+        vote: Number
     }
 
 }
@@ -29,4 +37,9 @@ export default {
 
 
 </script>
-<style></style>
+<style>
+.flags {
+    display: inline-block;
+    width: 15px;
+}
+</style>
